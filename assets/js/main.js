e1e61f0c63cdf92513c9d3b39c36e51552d19d6e
Maenhabes <Maenhabes@yahoo.com>
2021-05-11 01:27:54 -0500

    fixing bugs with project picture

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,8 +44,10 @@
             setTimeout(typeWriter2, speed3);
         }
     }
+
+
     let j = 0;
-    let txt2 = ' Please feel free to look at my projects inside my Project section, glad you\'re here!'; /* The text */
+    let txt2 = `Please feel free to look at my projects....  glad you\'re here!`; /* The text */
     var speed2 = 50; /* The speed/duration of the effect in milliseconds */
     function typeWriter3() {
         if (j < txt2.length) {
